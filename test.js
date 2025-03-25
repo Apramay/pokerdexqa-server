65a833689a609129c89a8486783d508db26b6ff1
Apramay <142184694+Apramay@users.noreply.github.com>
2025-03-24 23:33:30 -0400

    Update test.js

--- test.js
+++ test.js
@@ -209,7 +209,8 @@ function bettingRound(tableId) {
     return;
 } else if (
     nonAllInPlayers.length === 1 &&
-    table.playersWhoActed.has(nonAllInPlayers[0].name) 
+    table.playersWhoActed.has(nonAllInPlayers[0].name) &&
+    nonAllInPlayers[0].currentBet === table.currentBet
 ) {
     console.log("✅ Only one non-all-in player and they’ve acted. Moving to next round.");
     setTimeout(nextRound, 1000, tableId);
